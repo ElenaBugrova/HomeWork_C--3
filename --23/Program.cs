5f0ad470ce01c64253f0e1c436e0b23d6958120c
ElenaBugrova <evilisia@mail.ru>
2022-12-07 11:07:56 +0300

    домашка3

---
+++ --23/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу
+//кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+int number = 1;
+int index=N;
+while (index>0)
+{
+    double d=Math.Pow(number,3);
+    index--;
+    number++;
+    Console.Write($"{d}," );
+}
+
+
+//double d=Math.Pow(N,3);
+//Console.Write(d);
